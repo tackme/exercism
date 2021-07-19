d2329d0f31a118edd7c71f1d41a7a8ac22c00267
tackme <tackme7@gmail.com>
2021-07-20 01:42:32 +0900

    solved

---
+++ python/hamming/hamming.py
@@ -0,0 +1,11 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("not equal length of strands")
+
+    counter = 0
+
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            counter += 1
+
+    return counter
