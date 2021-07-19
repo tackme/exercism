0d37bfdb486086103c17624b725e3a931cd7b23b
tackme <tackme7@gmail.com>
2021-07-19 20:46:42 +0900

    fixed

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,23 +1,11 @@
 class Matrix:
     def __init__(self, matrix_string):
-        matrix_string = matrix_string.splitlines()
-
-        self.matrix = []
-
-        for item in matrix_string:
-            nums = item.split(" ")
-            row_int =[]
-            for num in nums:
-                row_int.append(int(num))
-            self.matrix.append(row_int)
+        self.matrix = [[int(num) for num in item.split(" ")] for item in matrix_string.splitlines()]
 
     def row(self, index):
         return self.matrix[index-1]
 
     def column(self, index):
-        column = []
-
-        for item in self.matrix:
-            column.append(item[index-1])
+        column = [item[index-1] for item in self.matrix]
 
         return column
