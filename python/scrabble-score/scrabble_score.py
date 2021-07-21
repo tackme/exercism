8cf5913141db8c29f4c77164fc1bf46bb4ca9d7b
tackme <tackme7@gmail.com>
2021-07-21 18:02:40 +0900

    fixed

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,22 +1,22 @@
 def score(word: str):
     letter_list = list(word.lower())
     
-    score = 0
+    count = 0
 
     for l in letter_list:
         if l in ("a", "e", "i", "o", "u", "l", "n", "r", "s", "t"):
-            score += 1
+            count += 1
         elif l in ("d", "g"):
-            score += 2
+            count += 2
         elif l in ("b", "c", "m", "p"):
-            score += 3
+            count += 3
         elif l in ("f", "h", "v", "w", "y"):
-            score += 4
-        elif l in ("k"):
-            score += 5
+            count += 4
+        elif l == "k":
+            count += 5
         elif l in ("j", "x"):
-            score += 8
+            count += 8
         elif l in ("q", "z"):
-            score += 10
+            count += 10
 
-    return score
+    return count
