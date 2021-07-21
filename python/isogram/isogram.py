860a0ca2747211d234ee1b3c9cc3f432468167e6
tackme <tackme7@gmail.com>
2021-07-21 16:29:27 +0900

    fixed

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,6 +1,5 @@
 import re
 
 def is_isogram(string):
-    lower_str = string.lower()
-    converted_str = re.sub("[ -]", "", lower_str)
+    converted_str = re.sub("[ -]", "", string.lower())
     return len(converted_str) == len(set(converted_str))
