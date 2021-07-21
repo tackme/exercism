4a5e364a3e22ebe8878d3d2ec0313f2da98c5097
tackme <tackme7@gmail.com>
2021-07-21 17:58:29 +0900

    fixed

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,17 +1,5 @@
 def score(word: str):
     letter_list = list(word.lower())
-
-    """
-    letter_values ={
-        ("a", "e", "i", "o", "u", "l", "n", "r", "s", "t"): 1,
-        ("d", "g"): 2,
-        ("b", "c", "m", "p"): 3,
-        ("f", "h", "v", "w", "y"): 4,
-        ("k"): 5,
-        ("j", "x"): 8,
-        ("q", "z"): 10
-        }
-    """
     
     score = 0
 
