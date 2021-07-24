e2de5471037e6d5cbe72b984c3597c7e0128e9fd
tackme <tackme7@gmail.com>
2021-07-24 23:08:15 +0900

    fixed

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,8 @@
 import re
 
 def count_words(sentence):
-    word_list = (re.findall("([a-zA-Z0-9]+(?:'[a-zA-Z0-9])?)", sentence.lower()))
+    word_list = (re.findall("([a-z0-9]+(?:'[a-z0-9])?)", sentence.lower()))
 
-    count = {}
-
-    for word in word_list:
-        if word not in count:
-            count[word] = 1
-        else:
-            count[word] += 1
+    count = {word: word_list.count(word) for word in word_list}
 
     return count
