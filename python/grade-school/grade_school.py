bb9e5bacfcdf898381f1ff7bec066e64100a7836
tackme <tackme7@gmail.com>
2021-07-30 19:04:52 +0900

    fixed

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,15 +1,15 @@
 class School:
     def __init__(self):
-        self.data = []
+        self._data = []
 
     def add_student(self, name, grade):
-        self.data.append({"Name": name, "Grade": grade})
-        self.data = sorted(self.data, key=lambda x: (x["Grade"], x["Name"]))
+        self._data.append({"Name": name, "Grade": grade})
+        self._data = sorted(self._data, key=lambda x: (x["Grade"], x["Name"]))
 
     def roster(self):
-        names = [d["Name"] for d in self.data]
+        names = [d["Name"] for d in self._data]
         return names
 
     def grade(self, grade_number):
-        names = [d["Name"] for d in self.data if d["Grade"] == grade_number]
+        names = [d["Name"] for d in self._data if d["Grade"] == grade_number]
         return names
