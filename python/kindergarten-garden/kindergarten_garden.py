ae03c84c2e8c19af7523b506adff814810dd9781
tackme <tackme7@gmail.com>
2021-08-01 22:10:04 +0900

    fixed

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,4 +1,11 @@
 class Garden:
+    PLANTS = {
+            "G" : "Grass",
+            "C" : "Clover",
+            "R" : "Radishes",
+            "V" : "Violets"
+            }
+
     def __init__(self, diagram, students=None):
         if students is None:
             students = [
@@ -15,19 +22,12 @@ class Garden:
                 "Kincaid",
                 "Larry"
                 ]
-        self.diagram = diagram.split()
+        self.garden = [list(l) for l in diagram.split()]
         self.students = sorted(students)
 
     def plants(self, name):
-        ref = {
-            "G" : "Grass",
-            "C" : "Clover",
-            "R" : "Radishes",
-            "V" : "Violets"
-            }
-
         index = self.students.index(name) * 2
 
-        reslut = [ref[m] for l in self.diagram for m in l[index:index+2]]
+        pots = self.garden[0][index:index+2] + self.garden[1][index:index+2]
 
-        return reslut
+        return [self.PLANTS[l] for l in pots]
