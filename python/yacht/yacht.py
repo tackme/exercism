a2007368a9fdd79bbf2dc9df368f82b80afd6c79
tackme <tackme7@gmail.com>
2021-08-21 18:39:14 +0900

    fixed yatch

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -10,7 +10,7 @@ THREES = lambda x: x[3] * 3
 FOURS = lambda x: x[4] * 4
 FIVES = lambda x: x[5] * 5
 SIXES = lambda x: x[6] * 6
-FULL_HOUSE = lambda x: sum(k * v for k, v in x.items()) if 2 and 3 in x.values() else 0
+FULL_HOUSE = lambda x: sum(k * v for k, v in x.items()) if len(x) == 2 and 3 in x.values() else 0
 FOUR_OF_A_KIND = lambda x: sum(l * 4 for l in x.keys() if x[l] > 3)
 LITTLE_STRAIGHT = lambda x: 30 if sum(x.keys()) == 15 else 0
 BIG_STRAIGHT = lambda x: 30 if sum(x.keys()) == 20 else 0
