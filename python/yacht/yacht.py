e803386994c1a66dd224a19d972b74e020cf1510
tackme <tackme7@gmail.com>
2021-08-21 18:13:48 +0900

    fixed yatch

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,18 +1,22 @@
+from collections import Counter
+
 # Score categories.
 # Change the values as you see fit.
-YACHT = lambda x: 50 if len(set(x)) == 1 else 0
-ONES = lambda x: sum(l for l in x if l == 1)
-TWOS = lambda x: sum(l for l in x if l == 2)
-THREES = lambda x: sum(l for l in x if l == 3)
-FOURS = lambda x: sum(l for l in x if l == 4)
-FIVES = lambda x: sum(l for l in x if l == 5)
-SIXES = lambda x: sum(l for l in x if l == 6)
-FULL_HOUSE = lambda x: sum(x) if len(set(x)) == 2 and any(x.count(l) == 3 for l in x) else 0
-FOUR_OF_A_KIND = lambda x: sum(l * 4 for l in set(x) if x.count(l) > 3)
-LITTLE_STRAIGHT = lambda x: 30 if sorted(x) == [1, 2, 3, 4, 5] else 0
-BIG_STRAIGHT = lambda x: 30 if sorted(x) == [2, 3, 4, 5, 6] else 0
-CHOICE = sum
 
+YACHT = lambda x: 50 if len(x) == 1 else 0
+ONES = lambda x: x[1]
+TWOS = lambda x: x[2] * 2
+THREES = lambda x: x[3] * 3
+FOURS = lambda x: x[4] * 4
+FIVES = lambda x: x[5] * 5
+SIXES = lambda x: x[6] * 6
+FULL_HOUSE = lambda x: sum(k * v for k, v in x.items()) if 2 and 3 in x.values() else 0
+FOUR_OF_A_KIND = lambda x: sum(l * 4 for l in x.keys() if x[l] > 3)
+LITTLE_STRAIGHT = lambda x: 30 if sum(x.keys()) == 15 else 0
+BIG_STRAIGHT = lambda x: 30 if sum(x.keys()) == 20 else 0
+CHOICE = lambda x: sum(k * v for k, v in x.items())
 
 def score(dice, category):
-    return category(dice)
+    dice_counter = Counter(dice)
+
+    return category(dice_counter)
