bb4c754d7e1ab9abdad100ec0e8be0d6557cea76
tackme <tackme7@gmail.com>
2021-08-31 02:45:40 +0900

    fixed

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,6 +1,10 @@
 def find_anagrams(word, candidates):
     letter_list = sorted(word.lower())
 
-    result = [l for l in candidates if sorted(l.lower()) == letter_list and word.lower() != l.lower()]
+    result = []
+
+    for candidate in candidates:
+        if sorted(candidate.lower()) == letter_list and word.lower() != candidate.lower():
+            result.append(candidate)
 
     return result
