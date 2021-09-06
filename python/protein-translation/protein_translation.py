480d65e7de5da53186d464264ffa69b58b19bbca
tackme <tackme7@gmail.com>
2021-09-06 20:40:15 +0900

    solved protein-translation

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from more_itertools.more import chunked
+
+
+class Protein(Enum):
+    AUG = "Methionine"
+    UUU = "Phenylalanine"
+    UUC = "Phenylalanine"
+    UUA = "Leucine"
+    UUG = "Leucine"
+    UCU = "Serine"
+    UCC = "Serine"
+    UCA = "Serine"
+    UCG = "Serine"
+    UAU = "Tyrosine"
+    UAC = "Tyrosine"
+    UGU = "Cysteine"
+    UGC = "Cysteine"
+    UGG = "Tryptophan"
+    UAA = "STOP"
+    UAG = "STOP"
+    UGA = "STOP"
+
+def proteins(strand):
+    chunked_strand = ["".join(chunk) for chunk in chunked(strand, 3)]
+
+    print(chunked_strand)
+    print(Protein["AUG"].value)
+
+    result = []
+
+    for chunk in chunked_strand:
+        if Protein[chunk].value == "STOP":
+            break
+
+        result.append(Protein[chunk].value)
+
+    return result
