4674ba9a26927557e5c14d3c922a88c645b0183a
tackme <tackme7@gmail.com>
2021-09-17 20:09:01 +0900

    fixed

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,17 +1,52 @@
 from typing import List
-from enum import Enum
+from enum import Enum, auto
 from more_itertools.more import chunked
 
+class Methionine(Enum):
+    AUG = auto()
 
-class Codon(Enum):
-    AUG = 'Methionine'
-    UUU = UUC = 'Phenylalanine'
-    UUA = UUG = 'Leucine'
-    UCU = UCC = UCA = UCG = 'Serine'
-    UAU = UAC = 'Tyrosine'
-    UGU = UGC = 'Cysteine'
-    UGG = 'Tryptophan'
-    UAA = UAG = UGA = 'STOP'
+class Phenylalanine(Enum):
+    UUC = auto()
+    UUU = auto()
+
+class Leucine(Enum):
+    UUA = auto()
+    UUG = auto()
+
+class Serine(Enum):
+    UCA = auto()
+    UCC = auto()
+    UCG = auto()
+    UCU = auto()
+
+class Tyrosine(Enum):
+    UAC = auto()
+    UAU = auto()
+
+class Cysteine(Enum):
+    UGC = auto()
+    UGU = auto()
+
+class Tryptophan(Enum):
+    UGG = auto()
+
+class Proteins(Enum):
+    methionine = Methionine
+    phenylalanine = Phenylalanine
+    leucine = Leucine
+    serine = Serine
+    tyrosine = Tyrosine
+    cysteine = Cysteine
+    tryptophan = Tryptophan
+
+    @classmethod
+    def from_codon(cls, codon):
+        for i in Proteins:
+            for j in i.value:
+                if codon in j.name:
+                    return type(j).__name__
+
+        return None
 
 def proteins(strand: str) -> List[str]:
     chunked_strand = ["".join(chunk) for chunk in chunked(strand, 3)]
@@ -19,9 +54,10 @@ def proteins(strand: str) -> List[str]:
     result = []
 
     for chunk in chunked_strand:
-        if Codon[chunk].value == "STOP":
-            break
+        if Proteins.from_codon(chunk):
+            result.append(Proteins.from_codon(chunk))
 
-        result.append(Codon[chunk].value)
+        else:
+            break
 
     return result
