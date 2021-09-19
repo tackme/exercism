ef53a4709a07020ba7eefa0f9f64bfcceebae849
tackme <tackme7@gmail.com>
2021-09-20 04:31:11 +0900

    fixed

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,13 @@
-def equilateral(sides):
+from typing import List
+
+
+def equilateral(sides: List[int]) -> bool:
     return len(set(sides)) == 1 and max(sides) < (sum(sides) - max(sides))
 
 
-def isosceles(sides):
+def isosceles(sides: List[int]) -> bool:
     return len(set(sides)) <= 2 and max(sides) < (sum(sides) - max(sides))
 
 
-def scalene(sides):
+def scalene(sides: List[int]) -> bool:
     return len(set(sides)) == 3 and max(sides) < (sum(sides) - max(sides))
