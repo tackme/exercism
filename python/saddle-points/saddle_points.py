8f209d719ef6537ca81838a68c64ed319df24f13
tackme <tackme7@gmail.com>
2021-09-20 04:00:50 +0900

    solve saddle-points

---
+++ python/saddle-points/saddle_points.py
@@ -0,0 +1,17 @@
+from typing import Dict, List
+
+def transpose(matrix: List[List[int]]) -> List[List[int]]:
+    return [list(x) for x in zip(*matrix)]
+
+def saddle_points(matrix: List[List[int]]) -> List[Dict[str, int]]:
+    if matrix != transpose(transpose(matrix)):
+        raise ValueError("This is not a matrix")
+
+    saddles = []
+
+    for row_index, row in enumerate(matrix):
+        for colmun_index, item in enumerate(row):
+            if item == max(row) and item == min(transpose(matrix)[colmun_index]):
+                saddles.append({"row": row_index + 1, "column":colmun_index + 1})
+
+    return saddles
