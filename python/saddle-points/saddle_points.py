801232559d90d661fdb2c9a7e0e856118a8980c4
tackme <tackme7@gmail.com>
2021-09-22 15:56:38 +0900

    fixed

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -7,10 +7,9 @@ def saddle_points(matrix: List[List[int]]) -> List[Dict[str, int]]:
     if matrix != transpose(transpose(matrix)):
         raise ValueError("This is not a matrix")
 
-
     return [
         {"row": row_index + 1, "column":colmun_index + 1}
         for row_index, row in enumerate(matrix)
-        for colmun_index, item in enumerate(row)
-        if item == max(row) and item == min(transpose(matrix)[colmun_index])
+        for colmun_index, colomn in enumerate(row)
+        if colomn == max(row) and colomn == min(transpose(matrix)[colmun_index])
     ]
