97e841f1584e09d2775cd3102e164717bfa45522
tackme <tackme7@gmail.com>
2021-09-23 18:10:18 +0900

    fixed

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,31 +1,39 @@
-from typing import List, Optional, Union
+from typing import Callable, Iterable, List, Optional, Tuple, Union
+from operator import gt, lt
 
 def is_palindrome(number: int) -> bool:
     return str(number) == str(number)[::-1]
+
 
 def check_factors(min_factor: int, max_factor: int):
     if min_factor > max_factor:
         raise ValueError("min_factor must be smaller than max_factor.")
 
-def largest(min_factor: int, max_factor: int) -> List[Union[Optional[int], List[List[int]]]]:
-    check_factors(min_factor, max_factor)
 
-    product = 0
+def find_palindrome(product: int, rng: Iterable[int], op: Callable[[int, int], bool]) -> Tuple[int, List[List[int]]]:
     factors = []
 
-    for i in range(max_factor, min_factor - 1, -1):
-        for j in range(max_factor, min_factor - 1, -1):
+    for i in rng:
+        for j in rng:
             n = i * j
 
-            if product > n:
+            if op(product, n):
                 break
 
             if is_palindrome(n):
-                if product < n:
+                if op(n, product):
                     product = n
                     factors = [sorted((i, j))]
                 elif product == n:
                     factors.append(sorted((i, j)))
+
+    return product, factors
+
+
+def largest(min_factor: int, max_factor: int) -> List[Union[Optional[int], List[List[int]]]]:
+    check_factors(min_factor, max_factor)
+
+    product, factors = find_palindrome(0, range(max_factor, min_factor - 1, -1), gt)
 
 
     return [product or None, factors]
@@ -33,22 +41,6 @@ def largest(min_factor: int, max_factor: int) -> List[Union[Optional[int], List[
 def smallest(min_factor: int, max_factor: int) -> List[Union[Optional[int], List[List[int]]]]:
     check_factors(min_factor, max_factor)
 
-    product = max_factor ** 2
-    factors = []
-
-    for i in range(min_factor, max_factor + 1):
-        for j in range(min_factor, max_factor + 1):
-            n = i * j
-
-            if product < n:
-                break
-
-            if is_palindrome(n):
-                if product > n:
-                    product = n
-                    factors = [sorted((i, j))]
-                elif product == n:
-                    factors.append(sorted((i, j)))
-
+    product, factors = find_palindrome(max_factor ** 2, range(min_factor, max_factor + 1), lt)
 
     return [product if is_palindrome(product) else None, factors]
